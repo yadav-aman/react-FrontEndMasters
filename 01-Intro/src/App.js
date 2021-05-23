203716320ebd7cb2545739c15de7448102f9285a
Aman Yadav <amansy04@gmail.com>
2021-05-23 18:37:21 +0530

    Removed redunddant code
    removed react source from cdn in index.html
    removed react.create element as we are using JSX in App and Pet

--- 01-Intro/src/App.js
+++ 01-Intro/src/App.js
@@ -1,48 +1,16 @@
 import React from "react";
 // we dont need to import react with JSX but it's not working with Parcel
 
-// import ReactDOM from "react-dom";
 import { render } from "react-dom";
-// import Pet from "./Pet";
 import SearchParams from "./SearchParams";
-
-// APP using JSX
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" /> */}
       <SearchParams />;
     </div>
   );
 };
 
 render(<App />, document.getElementById("root"));
-
-/* 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mix",
-    }),
-  ]);
-};
-
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
- */
--- 01-Intro/src/Pet.js
+++ 01-Intro/src/Pet.js
@@ -13,15 +13,3 @@ const Pet = (props) => {
 };
 
 export default Pet;
-
-/* 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
-
-export default Pet;
- */
