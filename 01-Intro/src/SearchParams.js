78ba00fc9eae5cd2b62dbd7a32539fcc745454e0
Aman Yadav <amansy04@gmail.com>
2021-05-23 13:06:18 +0530

    Hooks - useEffect

--- 01-Intro/src/SearchParams.js
+++ 01-Intro/src/SearchParams.js
@@ -1,13 +1,41 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, updateLocation] = useState("Seattle, WA");
+  //   const [location, updateLocation] = useState("Seattle, WA");
+  const [location, updateLocation] = useState("");
   const [animal, updateAnimal] = useState("");
   const [breed, updateBreed] = useState("");
+  const [pets, setPets] = useState([]);
   const breeds = [];
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  /* 
+  The [] at the end of the useEffect is where you declare your data dependencies. 
+  React wants to know when to run that effect again. You don't give it data dependencies, 
+  it assumes any time any hook changes that you should run the effect again. 
+  This is bad because that would mean any time setPets gets called it'd re-run render and all the hooks again. 
+  See a problem there? It'd run infinitely since requestPets calls setPets.
+  You can instead provide which hooks to watch for changes for. 
+  In our case, we actually only want it to run once, on creation of the component, 
+  and then to not run that effect again. (we'll do searching later via clicking the submit button) 
+  You can accomplish this only-run-on-creation by providing an empty array.
+   */
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
@@ -33,6 +61,7 @@ const SearchParams = () => {
             <option />
             {ANIMALS.map((animal) => (
               <option key={animal} value={animal}>
+                {/* Index is not a good optino for the key here */}
                 {animal}
               </option>
             ))}
@@ -58,6 +87,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
